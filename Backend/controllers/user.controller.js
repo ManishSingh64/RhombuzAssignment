80d3693fdce5745f850695c3266c985e874b253f
Dedpool1998 <msmsmanishkumar64@gmail.com>
2022-12-27 11:37:52 +0530

    pagination and sort done on frontend

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -3,24 +3,44 @@ const USER = require("../schema/user.schema");
 const userController = express.Router();
 
 userController.get("/", async (req, res) => {
-  let {sortByEmail, sortByGender,limit, page } = req.query;
+  let { sortByEmail, sortByGender, limit, page } = req.query;
+  let count = (await USER.find()).length;
   let data;
-  if(sortByEmail =='asc' && page){
-    data = await USER.find().sort({"email":1}).skip((page*limit)-limit).limit(limit)
-}else if(sortByEmail == 'desc' && page){
-    data = await USER.find().sort({"email":-1}).skip((page*limit)-limit).limit(limit)
-}
-    else if(sortByGender == 'asc' && page){
-        console.log('in')
-    data = await USER.find().sort({"gender":1}).skip((page*limit)-limit).limit(limit)
-}
-    else if(sortByGender == 'desc' && page){
-    data = await USER.find().sort({"gender":-1}).skip((page*limit)-limit).limit(limit)
-}else if(page){
-    data = await USER.find().skip((page*limit)-limit).limit(limit)
-}
-  
-  res.send(data);
+  if (sortByEmail == "asc" && sortByGender == "" && page) {
+    data = await USER.find()
+      .sort({ email: 1 })
+      .skip(page * limit - limit)
+      .limit(limit);
+  } else if (sortByEmail == "desc" && sortByGender == "" && page) {
+    data = await USER.find()
+      .sort({ email: -1 })
+      .skip(page * limit - limit)
+      .limit(limit);
+  } else if (sortByEmail == "" && sortByGender == "" && page) {
+    data = await USER.find()
+      .skip(page * limit - limit)
+      .limit(limit);
+  } else if (sortByGender == "asc" && sortByEmail == "" && page) {
+    data = await USER.find()
+      .sort({ gender: 1 })
+      .skip(page * limit - limit)
+      .limit(limit);
+  } else if (sortByGender == "desc" && sortByEmail == "" && page) {
+    data = await USER.find()
+      .sort({ gender: -1 })
+      .skip(page * limit - limit)
+      .limit(limit);
+  } else if (sortByGender == "" && sortByEmail == "" && page) {
+    data = await USER.find()
+      .skip(page * limit - limit)
+      .limit(limit);
+  }
+
+  res.send({
+    users: data,
+    totalPages: Math.ceil(count / limit),
+    currentPage: +page,
+  });
 });
 
 module.exports = userController;
--- src/components/Tablee.jsx
+++ src/components/Tablee.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import data from "../db.json";
 import {
   Table,
@@ -11,34 +11,92 @@ import {
   TableCaption,
   TableContainer,
 } from "@chakra-ui/react";
-import { ArrowUpDownIcon } from "@chakra-ui/icons";
+import {
+  ArrowUpDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icons";
+import axios from "axios";
 
 export const Tablee = () => {
-  const [tableData, setTableData] = useState(data);
+  const [tableData, setTableData] = useState();
+  const [sortEmail, setSortEmail] = useState("");
+  const [sortGender, setSortGender] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
 
-  console.log(tableData);
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:8080/user?sortByEmail=${sortEmail}&sortByGender=${sortGender}&page=${page}&limit=10`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setTableData(res.data.users);
+        setPage(res.data.currentPage);
+        setTotalPages(res.data.totalPages);
+      });
+  }, [sortEmail, sortGender, page]);
+  function sortEmailFunc() {
+    if (sortEmail == "") {
+      setSortEmail("asc");
+      setSortGender("");
+    } else if (sortEmail == "asc") {
+      setSortEmail("desc");
+      setSortGender("");
+    } else {
+      setSortEmail("");
+      setSortGender("");
+    }
+  }
+  function sortGenderFunc() {
+    if (sortGender == "") {
+      setSortGender("asc");
+      setSortEmail("");
+    } else if (sortGender == "asc") {
+      setSortGender("desc");
+      setSortEmail("");
+    } else {
+      setSortGender("");
+      setSortEmail("");
+    }
+  }
+  let paginationButtonEnable = {
+    border: "1px solid grey",
+    backgroundColor: "purple",
+    color: "white",
+    width: "30px",
+    borderRadius: "5px",
+  };
+  let paginationButtonDisable = {
+    border: "1px solid grey",
+    backgroundColor: "#8b6b96",
+    color: "white",
+    width: "30px",
+    borderRadius: "5px",
+    cursor:'not-allowed'
+  }
+  // console.log(tableData);
   return (
     <div>
       <TableContainer maxWidth="80%" margin="auto" border="1px solid gray.200">
         <Table variant="simple">
           <Thead bg="gray.100">
             <Tr>
+              <Th>First Name</Th>
+              <Th>Last Name</Th>
               <Th>
-                First Name <ArrowUpDownIcon cursor="pointer" />
+                Email{" "}
+                <ArrowUpDownIcon cursor="pointer" onClick={sortEmailFunc} />
               </Th>
               <Th>
-                Last Name <ArrowUpDownIcon cursor="pointer" />
-              </Th>
-              <Th>
-                Email <ArrowUpDownIcon cursor="pointer" />
-              </Th>
-              <Th>
-                Gender <ArrowUpDownIcon cursor="pointer" />
+                Gender{" "}
+                <ArrowUpDownIcon cursor="pointer" onClick={sortGenderFunc} />
               </Th>
             </Tr>
           </Thead>
-          {tableData.map((el) => (
-            <Tbody>
+          {tableData?.map((el) => (
+            <Tbody key={el._id}>
               <Tr>
                 <Td>{el.first_name}</Td>
                 <Td>{el.last_name}</Td>
@@ -49,6 +107,25 @@ export const Tablee = () => {
           ))}
         </Table>
       </TableContainer>
+      <div>
+        <h3>
+          Page {page} of <label style={{ color: "grey" }}>{totalPages}</label>
+        </h3>
+        <button
+          style={page == 1 ? paginationButtonDisable : paginationButtonEnable}
+          disabled={page == 1}
+          onClick={() => setPage((prev) => prev - 1)}
+        >
+          <ChevronLeftIcon />
+        </button>
+        <button
+          style={page == totalPages ? paginationButtonDisable : paginationButtonEnable}
+          disabled={page == totalPages}
+          onClick={() => setPage((prev) => prev + 1)}
+        >
+          <ChevronRightIcon />
+        </button>
+      </div>
     </div>
   );
 };
